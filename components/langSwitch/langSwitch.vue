6195aab0e75cfa707ae09efb604613a13361a723
civil.ahmad.eid <civil.ahmad.eid@gmail.com>
2022-11-09 23:46:46 +0200

    first upload

---
+++ components/langSwitch/langSwitch.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <span v-if="$i18n.locale === 'en'"><nuxt-link :to="switchLocalePath('ar')">العربية</nuxt-link></span>
+    <span v-if="$i18n.locale === 'ar'"><nuxt-link :to="switchLocalePath('en')">English</nuxt-link></span>
+  </div>
+</template>
+
+<style scoped lang="scss">
+span {
+  font-weight: 600;
+  margin: 0 10px;
+  a {
+    color: #000;
+  }
+}
+</style>
