8ee07143f7481c76231baffc1d5e3f9d859e6833
civil.ahmad.eid <civil.ahmad.eid@gmail.com>
2023-01-24 13:16:55 +0200

    merge

--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,14 @@
     <app-home-news :blogs="blogs" />
     <app-home-partners :partners="partners" />
     <app-home-bottom-banner :bottomBanner="bottomBanner" />
+    <SocialChat :attendants="attendants">
+      <p slot="header">Click one of our representatives below to chat.</p>
+      <template v-slot:button="{ open }">
+        <span v-show="!open">Contact us</span>
+        <span v-show="open">Close</span>
+      </template>
+      <small slot="footer">Opening hours: 8am to 10pm</small>
+    </SocialChat>
   </div>
 </template>
 
@@ -29,6 +37,62 @@ import AppHomeSteps from "../components/home/AppHomeSteps.vue";
 
 export default {
   name: "HomePage",
+  data() {
+    return {
+      attendants: [
+        {
+          app: "whatsapp",
+          label: "Support",
+
+          name: this.$store.state.websiteSettings.find(
+            (one) => one.key === "chat_widget_whatsapp_label"
+          )
+            ? this.$store.state.websiteSettings.find(
+                (one) => one.key === "chat_widget_whatsapp_label"
+              ).plain_value
+            : "Laravada",
+
+          number: this.$store.state.websiteSettings.find(
+            (one) => one.key === "chat_widget_whatsapp_number"
+          )
+            ? this.$store.state.websiteSettings.find(
+                (one) => one.key === "chat_widget_whatsapp_number"
+              ).plain_value
+            : "11111111111",
+
+          avatar: {
+            src: "https://avatars0.githubusercontent.com/u/8084606?s=460&u=20b6499a416cf7129a18e5c168cf387e159edb1a&v=4",
+            alt: "Laravada customer support",
+          },
+        },
+        {
+          app: "messenger",
+          label: "Technical support",
+
+          name: this.$store.state.websiteSettings.find(
+            (one) => one.key === "chat_widget_messenger_label"
+          )
+            ? this.$store.state.websiteSettings.find(
+                (one) => one.key === "chat_widget_messenger_label"
+              ).plain_value
+            : "Laravada Facebook",
+
+          id: this.$store.state.websiteSettings.find(
+            (one) => one.key === "chat_widget_messenger_id"
+          )
+            ? this.$store.state.websiteSettings.find(
+                (one) => one.key === "chat_widget_messenger_id"
+              ).plain_value
+            : "111111111111",
+
+          avatar: {
+            src: "https://avatars0.githubusercontent.com/u/8084606?s=460&u=20b6499a416cf7129a18e5c168cf387e159edb1a&v=4",
+            alt: "Laravada customer support",
+          },
+        },
+      ],
+    };
+  },
   async asyncData({ $axios, app }) {
     const slides = await $axios.get("/sliders", {
       headers: {
@@ -106,4 +170,16 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.home {
+  --vsc-bg-header: var(--main-color);
+  --vsc-bg-footer: var(--main-color);
+  --vsc-text-color-header: #000;
+  --vsc-text-color-footer: #000;
+  --vsc-bg-button: var(--main-color);
+  --vsc-text-color-button: #000;
+  --vsc-outline-color: var(--main-color);
+  --vsc-border-color-bottom-header: #fff;
+  --vsc-border-color-top-footer: #fff;
+}
+</style>
